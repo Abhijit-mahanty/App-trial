54f49aeea5d122147bbc0858763010df24b93c42
Abhijit-mahanty <mahantyabhijit29@gmail.com>
2026-07-12 12:04:21 +0530

    Fix logger and request/response serializers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -8,16 +8,16 @@ const app: Express = express();
 
 app.use(
   pinoHttp({
-    logger,
+    logger:logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
